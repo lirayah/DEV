bd5441024cf326eb0025f071d254d56b14925e9f
Lionel Morin <lionel.morin@etu.u-pec.fr>
2022-09-23 16:30:51 +0200

    MA BITE

---
+++ DEV1.1/TP9/figures.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void){
+    int a;
+    int b;
+    int c;
+    int d=0;
+    int e;
+    int f;
+    char s;
+    printf(" t) Triangle\n c) Carre\n q) Quitter\n");
+    s=getchar();
+    if(s=='t'){
+        printf("Hauteur? ");
+        scanf("%d",&b);
+        for(a=0;a<b;a++){
+            for(c=0;c<=d;c++){
+                printf("*");
+		}
+		d+=1;
+        printf("\n");
+      }
+    }
+    if (s=='c'){
+        printf("Hauteur? ");
+        scanf("%d",&b);
+        for(a=0;a<b;a++){
+            printf("*");
+        }
+            for(e=0;e<=b-3;e++){
+                printf("\n");
+                printf("*");
+                for(f=0;f<=b-3;f++){
+                    printf(" ");
+                }
+                printf("*");
+            }
+        printf("\n");
+        for(a=0;a<b;a++){
+            printf("*");
+        }printf("\n");
+    }
+    if(s=='q'){
+        return EXIT_SUCCESS;
+    }
+}
--- DEV1.1/TP9/sapin.c
+++ DEV1.1/TP9/sapin.c
@@ -4,10 +4,22 @@
 int main(void){
 	int a;
 	int b;
-	char s='*';
+	int c;
+	int d=0;
+	int i;
+	int e;
 	printf("Hauteur ? ");
 	scanf(" %d",&b);
-	for(a=0;a<=b;a++){
-		printf("%c\n", s);
+	e=b-1;
+	for(a=0;a<b;a++){
+		for (i=e;i>0;i--){
+			printf(" ");
+		} 
+		for(c=0;c<=d;c++){
+			printf("*");
+		}
+		d+=2;
+		e--;
+		printf("\n");
 	}
 }
