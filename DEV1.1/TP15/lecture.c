01435930711406fa343ec33a4b9a4e9a06905af7
morinl <login@domaine.example>
2022-10-10 15:58:55 +0200

    ez

--- DEV1.1/TP15/lecture.c
+++ DEV1.1/TP15/lecture.c
@@ -5,15 +5,16 @@
 int main(void){
     int i=0;
     int fin=0;
-    char mdp[27];
-    printf("Coucou");
+    char mdp[26];
+    printf("Coucou: \n");
     for(i=0;i<26 || fin==1;i++){
         mdp[i]=getchar();
-        if (mdp[i]==' '){
+        if (mdp[i]=='\n'){
+            mdp[i]='\0';
             fin=1;
             }
     }
-    if (mdp[0]=='p'&&mdp[1]=='a'&&mdp[2]=='s'&&mdp[3]=='t'&&mdp[4]=='a'){
+    if (mdp[26]=='p'&&mdp[1]=='a'&&mdp[2]=='s'&&mdp[3]=='t'&&mdp[4]=='a'){
         printf("GG WP T'AS HACKE LA MATRICE!");
     }
     else{
