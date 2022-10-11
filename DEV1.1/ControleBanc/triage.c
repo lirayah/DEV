c124555412bd7bc557e521b55b0a2d5db448ac85
morinl <login@domaine.example>
2022-10-11 15:58:31 +0200

    ez

--- DEV1.1/ControleBanc/triage.c
+++ DEV1.1/ControleBanc/triage.c
@@ -4,7 +4,7 @@
 int main(void){
     double a=2.0;
     int b=02;
-    float c=0x32;
+    char c=0x32;
     long int d=2L;
-    printf("%.1f\n%2d\n%f\n%lu\n",a,b,c,d);
+    printf("%.0f\n%o\n%c\n%ld\n",a,b,c,d);
 }
