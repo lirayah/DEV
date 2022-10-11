854082136af59706b4f5aacbc3239e3d38a29662
morinl <login@domaine.example>
2022-10-11 16:23:25 +0200

    ez

--- DEV1.1/ControleBanc/histogramme.c
+++ DEV1.1/ControleBanc/histogramme.c
@@ -2,5 +2,20 @@
 #include<stdio.h>
 
 int main(void){
-    int a;
+    int i;
+    int f;
+    int tab[19];
+    for(i=0;i<19;i++){
+        f=0;
+        printf("Nombre %d ",i+1);
+        scanf("%d",&tab[i]);
+    }
+    for(i=0;i<19;i++){
+        f=0;
+        while (f<tab[i]){
+            printf("▄");
+            f=f+1;
+        }
+        printf("\n");
+    }
 }
