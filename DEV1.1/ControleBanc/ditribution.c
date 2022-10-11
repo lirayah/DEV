4df37295226ff15fc1d62673073b9f19b53c44ee
morinl <login@domaine.example>
2022-10-11 17:10:17 +0200

    ez

--- DEV1.1/ControleBanc/ditribution.c
+++ DEV1.1/ControleBanc/ditribution.c
@@ -3,7 +3,26 @@
 #include<time.h>
 
 int main(void){
-    srand(time(NULL))
+    srand(time(NULL));
+    RAND_MAX=1;
     int a;
-    
+    int b=10;
+    int i;
+    int j;
+    int nb;
+    int tab[4]
+    printf("Nombre de tests: ");
+    scanf("%d",&nb);
+    for(i=0;i<nb;i++){
+        for(j=0;j<3;j++){
+            a=rand();
+            if(a==0){
+                b--;
+            }
+            else{
+                b++;
+            }
+        }
+        if b
+    }
 }
