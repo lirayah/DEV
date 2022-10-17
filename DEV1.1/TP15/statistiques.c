6423c809135400b5874c87d470ac540eb638c586
morinl <login@domaine.example>
2022-10-17 16:15:58 +0200

    ez

--- DEV1.1/TP15/statistiques.c
+++ DEV1.1/TP15/statistiques.c
@@ -6,6 +6,7 @@ int main(void){
     int a=0;
     int b=200;
     int i;
+    int n;
     char tab[200];
     printf("phrase: ");
     scanf("%200s",tab);
@@ -13,6 +14,14 @@ int main(void){
     for(i=0;i<200;i++){
         if(tab[i]=='e'){
             a++;
+        }
+    }
+    for(i=0;i<200;i++){
+        for(n=i;n<200;n++){
+//tester a partir du i pas déjà fait sinon double decre
+            if(tab[i]==tab[n](
+                b--;
+            )        
         }
     }
     printf("%d\n",a);
