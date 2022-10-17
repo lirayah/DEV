f1223b7d5afb935c780c7c1e0b82bf6d3e5f6f98
morinl <login@domaine.example>
2022-10-17 15:47:31 +0200

    ez

--- DEV1.1/TP15/statistiques.c
+++ DEV1.1/TP15/statistiques.c
@@ -3,6 +3,15 @@
 #include<string.h>
 
 int main(void){
+    int a=0;
+    int i;
     char tab[200];
-    
+    printf("phrase");
+    scanf("%200s",tab);
+    printf("%s\n",tab);
+    for(i=0;i<199;i++){
+        if(tab[i]=e){
+            a++;
+        }
+    }
 }
