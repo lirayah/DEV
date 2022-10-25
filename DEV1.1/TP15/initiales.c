b510e2101f697c7f02624533ba247fcd1bcfc27e
morinl <login@domaine.example>
2022-10-25 16:33:28 +0200

    ez

--- DEV1.1/TP15/initiales.c
+++ DEV1.1/TP15/initiales.c
@@ -3,5 +3,10 @@
 #include<string.h>
 
 int main(int argc, char** argv){
-    int i=0;
+    int a=argc;
+    int i=1;
+    for(i;i<a;i++){
+        printf("%c",argv[i][0]);
+    }
+    printf("\n");
 }
