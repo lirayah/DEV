a3418830fb06223c6ac6f2ee9516c61b69ee8382
morinl <login@domaine.example>
2022-10-25 17:13:06 +0200

    ez

--- DEV1.1/TP15/multiplication.c
+++ DEV1.1/TP15/multiplication.c
@@ -3,13 +3,14 @@
 #include<string.h>
 
 int main(int argc, char** argv){
+    char txt[50];
     char* position1;
     char* position2;
     unsigned long ent1;
     unsigned long ent2;
     ent1=strtoul(argv[1],&position1,10);
     ent2=strtoul(argv[2],&position2,10);
-    if(((ent1==0) && (position1==argv[1])) || ((ent2==0)&&(position2==argv[2]))){
+    if(((ent1==0) && (position1==txt)) || ((ent2==0)&&(position2==txt))){
         printf("FK IT");
     }
     long a=strtol(argv[1],NULL,10);
