0ffa7e955ed2a5970600a942b8295eaad9f9009b
morinl <login@domaine.example>
2022-11-15 16:10:24 +0100

    ez

---
+++ DEV1.1/TP19/palindromes.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+char inverse(const char* s){
+    .
+}
+
+int main(char s[]){
+    char* s = NULL;
+    char* inverse(const char* s);
+}
--- DEV1.1/TP19/singletons.c
+++ DEV1.1/TP19/singletons.c
@@ -12,7 +12,7 @@ int main(void){
     scanf("%d", &taille);
     tab = (float*) malloc(taille*sizeof(float));
     for(i=0;i<taille;i++){
-        scanf(" %lf", &tab[i]);
+        scanf(" %f", &tab[i]);
     }
     for(i=0;i<taille;i++){
         drapeau=1;
@@ -21,8 +21,13 @@ int main(void){
                 drapeau=0;
             }
         }
+        for(j=i+1;j<taille;j++){
+            if (tab[i]==tab[j]){
+                drapeau=0;
+            }
+        }
         if (drapeau==1){
-            printf("%f",tab[i]);
+            printf("%f\n",tab[i]);
         }
     }
     free(tab);
