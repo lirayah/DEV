f5c04cb133afabdefa56be0cc63c78e590780d72
Lionel Morin <lionel.morin@etu.u-pec.fr>
2023-01-23 19:09:32 +0100

    Update 'DEV1.1/TP22/compteur.c'

--- DEV1.1/TP22/compteur.c
+++ DEV1.1/TP22/compteur.c
@@ -2,10 +2,17 @@
 #include<stdlib.h>
 
 int main(void){
-    FILE *flux;
+    FILE* flux=fopen("./a","r");
+    int compteur=0;
+    if(flux){
+        fread(&compteur,4,1,flux);
+    }
+    fclose(flux);
+    compteur++;
+    printf("%d\n",compteur);
     flux=fopen("./a","w");
     if(flux){
-        
-        fclose(flux);
+        fwrite(&compteur,4,1,flux);
     }
+    fclose(flux);
 }
