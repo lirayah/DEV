e1791b60c7d940d0ddfe05f8329d6feb62040e31
Lionel Morin <lionel.morin@etu.u-pec.fr>
2023-01-23 16:42:02 +0100

    Update 'DEV1.1/TP19/palindromes.c'

--- DEV1.1/TP19/palindromes.c
+++ DEV1.1/TP19/palindromes.c
@@ -2,10 +2,20 @@
 #include<stdlib.h>
 
 char inverse(const char* s){
-    
+    char* inv_str = (char*) calloc(strlen(s), sizeof(char));
+	for (int i = strlen(s)-1; i >= 0; i--) {
+		inv_str[i] = s[strlen(s)-i-1];
+	}
+	return inv_str;
 }
 
-int main(char s[]){
-    char* s = NULL;
-    char inverse(const char* s);
+int main(int argc, char* argv[]){
+    for(int i=1; i<argc;i++){
+        if (strcmp(argv[i], inverse(argv[i])) == 0){
+            printf("%s est palindrome.\n", argv[i]);
+        }
+		else{
+            printf("%s n'est pas palindrome.\n", argv[i]);
+        }
+    }
 }
