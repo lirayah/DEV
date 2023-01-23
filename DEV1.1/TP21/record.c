dad1e901ad105c0db2f8b704987b72bb5e8f78b9
Lionel Morin <lionel.morin@etu.u-pec.fr>
2023-01-23 17:30:10 +0100

    Update 'DEV1.1/TP21/record.c'

--- DEV1.1/TP21/record.c
+++ DEV1.1/TP21/record.c
@@ -2,13 +2,23 @@
 #include<stdlib.h>
 
 int main(void){
-    FILE *fichier=fopen("./top10","r");
-    int oui[50];
+    FILE* fichier=fopen("./top10","r");
+    char name1[4],name2[4],name3[4];
     int i=0;
+
     if(fichier){
-        while(fread(oui,sizeof(7),1,fichier)!=0){
-            printf("%d\n",oui);
-            i++;
+        while(!feof(fichier)){
+            int score=NULL;
+            char* name=calloc(3,sizeof(char));
+            fread(&score,4,1,fichier);
+            fread(name,1,3,fichier);
+            if(feof(fichier)){
+                break;
+            }
+            strcpy(name3,name2);
+            strcpy(name2,name1);
+            strcpy(name1,name);
+            //faire la meme chose avec les scores (en utilisant des tableaux) et print
         }
     }
     fclose(fichier);
