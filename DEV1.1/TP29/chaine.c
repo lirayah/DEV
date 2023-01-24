062d9f8f762dafddb00273fb7982bd83fe799b34
Lionel Morin <lionel.morin@etu.u-pec.fr>
2023-01-24 23:13:07 +0100

    Update 'DEV1.1/TP29/chaine.c'

--- DEV1.1/TP29/chaine.c
+++ DEV1.1/TP29/chaine.c
@@ -1,6 +1,41 @@
-#include<stdlib.h>
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(void){
-    
+struct maillon_ {
+    char c;
+    struct maillon_* prev;
+};
+typedef struct maillon_ maillon;
+typedef maillon* pile;
+
+void push(pile* p, char c) {
+    maillon* m = calloc(1, sizeof(maillon));
+    m->c = c;
+    m->prev = *p;
+    *p = m;
+}
+
+char pop(pile* p) {
+    char c = (*p)->c;
+    *p = (*p)->prev;
+    return c;
+}
+
+int empty(pile p) {
+    return p == NULL;
+}
+
+char top(pile p) {
+    return p->c;
+}
+
+void clear(pile* p) {
+    *p = NULL;
+}
+pile create() {
+    return NULL;
+}
+
+void destroy(pile* p) {
+    free(p);
 }
