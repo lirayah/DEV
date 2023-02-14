517257cf00e3a7683545654bffa583df5b01f4ff
morinl <login@domaine.example>
2023-02-14 16:18:18 +0100

    bvfdb

--- DEV2.1/TP4/Date2.java
+++ DEV2.1/TP4/Date2.java
@@ -3,6 +3,6 @@ package TP4;
 public class Date2 {
     public static void main(String[] args) {
         Date a= new Date();
-        System.out.println(a);
+        System.out.println("La date est:"+a);
     }
 }
--- DEV2.1/TP4/Lendemains.java
+++ DEV2.1/TP4/Lendemains.java
@@ -1,7 +1,46 @@
 package TP4;
 
 public class Lendemains {
-    public static void main(String[] args) {
-        
+    private int annee;
+    private int mois;
+    private int jour;
+    
+    public Lendemains(){
+        this.annee=2023;
+        this.mois=02;
+        this.jour=14;
+    }
+    public void jourApr(){
+        this.jour++;
+        if(this.jour==29 && this.mois==2){
+            this.jour=1;
+            this.mois++;
+        }
+        if(this.jour==31 && this.mois%2==0 && this.mois<8){
+            this.jour=1;
+            this.mois++;
+        }
+        if(this.jour==31 && this.mois%2==1 && this.mois>8){
+            this.jour=1;
+            this.mois++;
+        }
+        if(this.jour==32 && this.mois%2==1 && this.mois<8){
+            this.jour=1;
+            this.mois++;
+        }
+        if(this.jour==32 && this.mois%2==0 && this.mois>8){
+            this.jour=1;
+            this.mois++;
+        }
+        if(this.mois==12){
+            this.jour=1;
+            this.mois=1;
+            this.annee++;
+        }
+
+    }
+    public String toString() {
+        return Integer.toString(this.annee) + "-" + Integer.toString(this.mois) + "-" + Integer.toString(this.jour);
     }
 }
+
