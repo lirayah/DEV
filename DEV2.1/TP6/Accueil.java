ac287d7c839bc0f48dfc88d2fb3558a2cb0f6231
Lirayah <login@domaine.example>
2023-03-17 11:49:43 +0100

    nji

--- DEV2.1/TP6/Accueil.java
+++ DEV2.1/TP6/Accueil.java
@@ -8,8 +8,6 @@ public class Accueil {
         JFrame fenetre=new JFrame();
         fenetre.setSize(500,500);
         fenetre.setLocation(0,0);
-        GridLayout gestio=new GridLayout(2,1);
-        fenetre.setLayout(gestio);
         fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Color bgColor=new Color(212, 115, 212);
         fenetre.getContentPane().setBackground(bgColor);
