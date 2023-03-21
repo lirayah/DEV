3c54f601ab37405266147b90e4a226c9725cb68e
Lirayah <login@domaine.example>
2023-03-21 11:07:53 +0100

    fzjoo

--- DEV2.1/TP7/Moyenne.java
+++ DEV2.1/TP7/Moyenne.java
@@ -1,5 +1,24 @@
 package TP7;
 
-public class Moyenne {
+public abstract class Moyenne {
+    private Moyenne a;
+    private int j;
+
+    public Moyenne(){
+        Moyenne a;
+        int j;
+    }
+    public abstract byte add(byte b);
+
+    public abstract short add(short s);
     
+    public abstract int add(int i);
+    
+    public abstract long add(long l);
+
+    public abstract float add(float f);
+
+    public abstract double add(double d);
+
+    public abstract float getAverage();
 }
