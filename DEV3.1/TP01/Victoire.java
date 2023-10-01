788cc68730c81fd855fb92221440a8e18877e7c3
Morin <lionel.morin@etu.u-pec.fr>
2023-10-01 15:35:21 +0200

    vsdffuisd

--- DEV3.1/TP01/Victoire.java
+++ DEV3.1/TP01/Victoire.java
@@ -1,3 +1,14 @@
-public class Victoire {
-    
+import javax.swing.JFrame;
+
+public class Victoire extends JFrame{
+    public Victoire(){
+        super();
+        this.setSize(400, 400);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Victoire v = new Victoire();
+    }
 }
