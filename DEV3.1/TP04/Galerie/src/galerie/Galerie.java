1a952a63bc4902fd4293134de10041b99a7d2fb5
morinl <login@domaine.example>
2023-11-13 18:13:11 +0100

    xfd

---
+++ DEV3.1/TP04/Galerie/src/galerie/Galerie.java
@@ -0,0 +1,8 @@
+package galerie;
+
+public class Galerie {
+    public static void main(String[] args) {
+        Vue v = new Vue();
+        v.setVisible(true);
+    }
+}
---
+++ DEV3.1/TP04/Quizz/src/quizz/ControlleurMain.java
@@ -0,0 +1,8 @@
+package quizz;
+
+
+public class ControlleurMain {
+    public static void main(String[] args) {
+        
+    }    
+}
